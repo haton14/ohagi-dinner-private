db96b1fc30cc6aad6d2b60a5ecbd808232fb79f3
haton14 <iioka.takumi.quena@gmail.com>
2023-01-18 23:55:02 +0900

    Cloudflare Workerを使用するように修正

--- ohagi-dinner-front/src/app.d.ts
+++ ohagi-dinner-front/src/app.d.ts
@@ -1,18 +1,14 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
+/// <reference types="@sveltejs/kit" />
+/// <reference types="@sveltejs/adapter-cloudflare-workers" />
+
 declare namespace App {
 	// interface Error {}
-	interface Locals { }
+	// interface Locals {}
 	// interface PageData {}
 	interface Platform {
-		env: {
-			COUNTER: DurableObjectNamespace;
-		};
-		context: {
-			waitUntil(promise: Promise<any>): void;
-		};
-		caches: CacheStorage & { default: Cache }
+	  env: {
+		YOUR_KV_NAMESPACE: KVNamespace
+		YOUR_DURABLE_OBJECT_NAMESPACE: DurableObjectNamespace
+	  }
 	}
-
-}
+  }
--- ohagi-dinner-front/svelte.config.js
+++ ohagi-dinner-front/svelte.config.js
@@ -1,5 +1,6 @@
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-cloudflare';
+//import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-cloudflare-workers';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */

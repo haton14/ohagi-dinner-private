9ec6b270c3e7876412b4305b2fff051877070de3
haton14 <iioka.takumi.quena@gmail.com>
2023-01-26 23:03:56 +0900

    debug

--- ohagi-dinner-api/docker-entrypoint.sh
+++ ohagi-dinner-api/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'start litestream'
-cat $GOOGLE_APPLICATION_CREDENTIALS
+echo $GOOGLE_APPLICATION_CREDENTIALS
 litestream restore -o dinner.db gcs://ohagi-dinner-private-storage/dinner.db
 litestream replicate -exec "/app/ohagi-dinner-api"

e93dd7910a6eeeff59801b63bc0a5e97c4595ab1
haton14 <iioka.takumi.quena@gmail.com>
2023-01-26 23:58:24 +0900

    不要なデプロイ処理削除

--- ohagi-dinner-api/docker-entrypoint.sh
+++ ohagi-dinner-api/docker-entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo 'start litestream'
-echo $GOOGLE_APPLICATION_CREDENTIALS
-cat $GOOGLE_APPLICATION_CREDENTIALS
 litestream restore -o dinner.db gcs://ohagi-dinner-private-storage/dinner.db
 litestream replicate -exec "/app/ohagi-dinner-api"
